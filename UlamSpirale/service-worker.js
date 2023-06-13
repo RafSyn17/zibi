f79b256bbf99da32f03a50a6f23e16f4848a5f40
Rafael Synowiec <rafael.synowiec@gmail.com>
2023-06-13 19:51:05 +0200

    Ulam Offline mit developers.google.com Vorschlag
    https://developers.google.com/codelabs/pwa-training/pwa03--going-offline#3

--- UlamSpirale/service-worker.js
+++ UlamSpirale/service-worker.js
@@ -1,48 +1,31 @@
-// Name des Cache-Speichers
-var cacheName = 'meine-pwa-cache';
-
-// Dateien, die im Cache gespeichert werden sollen
-var filesToCache = [
-    './',
-    './index.html',
-    'https://rsy17.de/zibi/UlamSpirale/index.html'
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = [
+  './',
+  './index.html',
+  'https://rsy17.de/zibi/UlamSpirale/index.html'
 ];
 
-// Installation des Service Workers
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(function(cache) {
-        return cache.addAll(filesToCache);
-      })
-  );
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
-// Aktivierung des Service Workers
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys()
-      .then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(name) {
-            if (name !== cacheName) {
-              return caches.delete(name);
-            }
-          })
-        );
-      })
-  );
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
 });
 
-// Intercept fetch requests
-self.addEventListener('fetch', function(event) {
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
   );
 });
