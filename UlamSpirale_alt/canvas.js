ea853092eddfb9143906a3c5cea951dc406f36c1
Zbigniew Synowiec <zbigniew.sy@gmail.com>
2023-07-16 18:34:17 +0200

    Kosmetische Änderungen u.A. Checkbox

---
+++ UlamSpirale_alt/canvas.js
@@ -0,0 +1,57 @@
+function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+let j = 1;
+//let j_max = 3;
+function MaleUlamSpirale() {
+    var Canvas = document.getElementById("MeinCanvas");
+    var ctx = Canvas.getContext("2d");
+    ctx.strokeStyle = "rgb(0, 0, 255)"; // blau
+    ctx.fillStyle = "rgb(0, 255, 0)"; // grün
+
+    if (j < 2) j = 2;
+    let i = (j - 2) * 1441201 + 2;
+    let n = MeinCanvas.width;
+    let m = MeinCanvas.height;
+    ctx.clearRect(0, 0, n, m);
+    ctx.strokeRect(0, 0, n, m);
+
+    const center = Math.floor(n / 2);
+    let x = center;
+    let y = center;
+    let dx = 1;
+    let dy = 1;
+
+    for (let j = 1; j <= n; j++) {
+        for (let k = 1; k <= 2 * j; k++) {
+
+            if (k <= j) {
+                x += dx
+            }
+            else {
+                y += dy
+            }
+
+            if (isPrime(i)) {
+                ctx.fillRect(x, y, 1, 1);
+            }
+
+            i++;
+        }
+
+        dx = -dx;
+        dy = -dy;
+    }
+
+    // j++;
+    //if (j <= j_max)
+    //    setTimeout(MaleUlamSpirale, 1000);
+
+}
+
+MaleUlamSpirale();
